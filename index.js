0e4098f702727c671e95ae02195f4292871a8944
tuwana-ibrahim <tuwana.kalary@gmail.com>
2020-07-23 16:18:45 +0300

    My Personal Website

---
+++ index.js
@@ -0,0 +1,24 @@
+var items = document.getElementsByClassName("links");
+
+for (var i = 0; i < items.length; i++) {
+  items[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
