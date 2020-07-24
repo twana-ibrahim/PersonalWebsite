e7c7f98a1fd2103b1cc64a5bbbd093251fe4f734
tuwana-ibrahim <tuwana.kalary@gmail.com>
2020-07-25 01:19:50 +0300

    Pre Final Version

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var items = document.getElementsByClassName("links");
 
+
 for (var i = 0; i < items.length; i++) {
+
   items[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
   current[0].className = current[0].className.replace(" active", "");
@@ -9,17 +11,27 @@ for (var i = 0; i < items.length; i++) {
 }
 
 
-// var coll = document.getElementsByClassName("collapsible");
-// var i;
 
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.display === "block") {
-//       content.style.display = "none";
-//     } else {
-//       content.style.display = "block";
-//     }
-//   });
+
+
+
+// change navbar by scroll 
+// var divs = document.querySelectorAll(".rightSide");
+
+// var x = document.getElementsByClassName("rightSide")[0];
+
+// var divId = divs.id;
+
+// for(var i = 0; i < x.children.length; i++){
+//   var divsId = x.children[i].id;
+//   if(window.scrollX >= divsId[i]){
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     items.className += " active";
+//   }
+// }
+
+
+// if(window.scrollY >= divId = document.getElementById("skills"));{
+  // console.log(x.children[0].offset());
 // }
